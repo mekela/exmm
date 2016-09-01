5e4d8c847e1c3a0ded116a216545dcc09d35f282
alex <sania_free@i.ua>
2016-09-01 18:18:54 +0300

    2 pages

--- js/common.js
+++ js/common.js
@@ -7,6 +7,57 @@ $(document).ready(function() {
 			}
 		}
 	});
+
+	$('.datepicker').datepicker({
+		language: 'ru'
+	})
+
+	//slider
+	$('.product_info_slider ul').bxSlider();
+
+
+	/*----------------        footerBottom         --------------------*/
+	function footerBottom(){
+		var $this = $('.footer'),
+		height = parseInt($this.innerHeight());
+		$('.wrapper').css({
+			'padding-bottom': height + 'px',
+		});
+		$this.css({
+			'margin-top': '-' + height + 'px',
+		});
+	}
+	$(window).resize(function(event) {
+		footerBottom();
+	});
+	footerBottom();
+
+	/*----------------        sidebar         --------------------*/
+	function sidebar(){
+		var $this = $('.sidebar');
+		$this.css({
+			'min-height': $('.main').innerHeight()
+		})
+		$('.main').css({
+			'min-height': '720px'
+		})
+	}
+	$(window).resize(function(event) {
+		sidebar();
+	});
+	sidebar();
+	
+	/*----------------        sidebar navbar         --------------------*/
+	$('.sidebar-navbar').click(function(event) {
+		$('.sidebar').toggleClass('active');
+	});
+
+	$(document).click( function(event){
+      if( $(event.target).closest('.sidebar-navbar').length ) 
+        return;
+      $('.sidebar').removeClass('active');
+      event.stopPropagation();
+    });
 	
 
 });
